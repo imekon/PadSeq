f69bcf9e4f56e17f8f3508c25caee3780d5a0009
Pete Goodwin <pgoodwin@imekon.org>
2017-09-20 06:53:21 +0100

    split model into mapper which deals with bar and zoom

---
+++ Source/PadMapper.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// Deals with zoom, bar, and song mode
+class PadMapper
+{
+public:
+	PadMapper() {}
+	virtual ~PadMapper() {}
+
+
+};
--- Source/PadModel.h
+++ Source/PadModel.h
@@ -1,12 +1,12 @@
 #pragma once
 
+// Deals wityh cells in the pad, their location and colour
 class PadModel
 {
 public:
 	PadModel() {}
 	virtual ~PadModel() {}
 
-	virtual int getBarCount() = 0;
 	virtual int getNoteCount() = 0;
 };
 
